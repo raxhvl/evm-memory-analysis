99f1781203fa9aedde14a3dc4651d83cbca3e2db
rahul <raxhvl@users.noreply.github.com>
2024-09-21 14:09:38 +0000

    ✨ feat: New opcodes

--- memory-tracer/tracer/config.py
+++ memory-tracer/tracer/config.py
@@ -17,21 +17,123 @@ RPC_ENDPOINT = os.getenv("RPC_ENDPOINT")
 API_KEY = os.getenv("RPC_API_KEY")
 
 
+# There are two types of instructions:
+# 1. Instruction that access a fixed size of memory, in which case the dict below
+#    has a `size`.
+# 2. Instructions that access a variable size of memory, in which case the size is
+#    read from stack using `stack_input_positions`.
 INSTRUCTIONS = {
-    "MLOAD": "R",  # "R" (for "[R]ead")
-    "MSTORE": "W",  # "W" (for "write [W]ord")
-    "MSTORE8": "B",  # "B" (for "write [B]yte")
-    "CALLDATACOPY": "L",  # "L" (for "copy cal[L]data")
-    "CODECOPY": "D",  # "D" (for "copy co[D]e")
-    "EXTCODECOPY": "X",  # "X" (for "copy e[X]ternal code")
-    "RETURNDATACOPY": "N",  # "N" (for "copy retur[N] data")
-    "MCOPY": "M",  # "M" (for "copy [M]emory")
-    "RETURN": "U",  # "U"  (for "Ret[U]rn")
-    # STOP and CALL* instructions are inert. They are required to capture frame
-    # boundaries. These are ignored during memory analysis
-    "STOP": "I",
-    "CALL": "I",
-    "CALLCODE": "I",
-    "DELEGATECALL": "I",
-    "STATICCALL": "I",
+    "KECCAK256": {
+        "opcode": "20",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
+    "CALLDATACOPY": {
+        "opcode": "37",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 2}],
+    },
+    "CODECOPY": {
+        "opcode": "39",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 2}],
+    },
+    "MLOAD": {
+        "opcode": "51",
+        "access_size": "fixed",
+        "stack_input_positions": [{"offset": 0}],
+        "size": 32,
+    },
+    "MSTORE": {
+        "opcode": "52",
+        "access_size": "fixed",
+        "stack_input_positions": [{"offset": 0}],
+        "size": 32,
+    },
+    "MSTORE8": {
+        "opcode": "53",
+        "access_size": "fixed",
+        "stack_input_positions": [{"offset": 0}],
+        "size": 8,
+    },
+    "EXTCODECOPY": {
+        "opcode": "3c",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 1, "size": 3}],
+    },
+    "RETURNDATACOPY": {
+        "opcode": "3e",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 2}],
+    },
+    "MCOPY": {
+        "opcode": "5e",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 2}, {"offset": 1, "size": 2}],
+    },
+    "LOG0": {
+        "opcode": "a0",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
+    "LOG1": {
+        "opcode": "a1",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
+    "LOG2": {
+        "opcode": "a2",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
+    "LOG3": {
+        "opcode": "a3",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
+    "LOG4": {
+        "opcode": "a4",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
+    "CREATE": {
+        "opcode": "f0",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 1, "size": 2}],
+    },
+    "CALL": {
+        "opcode": "f1",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 3, "size": 4}, {"offset": 5, "size": 6}],
+    },
+    "CALLCODE": {
+        "opcode": "f2",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 3, "size": 4}, {"offset": 5, "size": 6}],
+    },
+    "RETURN": {
+        "opcode": "f3",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
+    "DELEGATECALL": {
+        "opcode": "f4",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 2, "size": 3}, {"offset": 4, "size": 5}],
+    },
+    "CREATE2": {
+        "opcode": "f5",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 1, "size": 2}],
+    },
+    "STATICCALL": {
+        "opcode": "fa",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 2, "size": 3}, {"offset": 4, "size": 5}],
+    },
+    "REVERT": {
+        "opcode": "fa",
+        "access_size": "variable",
+        "stack_input_positions": [{"offset": 0, "size": 1}],
+    },
 }
